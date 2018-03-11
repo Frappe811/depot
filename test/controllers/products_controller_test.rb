aed06df3564d8244d7652193ade959714e1e642b
Frappe811 <monichan811@gmail.com>
2018-03-11 23:42:41 +0700

    Check if user create a book with duplicate title

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -22,6 +22,13 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create product" do
+    assert_difference('Product.count', 0) do
+      post products_url, params: { product: { 
+                    description: @product.description,
+                    image_url: @product.image_url,
+                    price: @product.price,
+                    title: @product.title }  }
+    end
     assert_difference('Product.count') do
       post products_url, params: { product: @update }
     end
